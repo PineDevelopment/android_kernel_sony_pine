0c893bdd26c2c140e2ca915e5560b195f79c8dd9
Andrew <poisk567@gmail.com>
2019-01-18 18:10:13 +0200

    pine: patched binder for oreo

--- drivers/staging/android/sync_debug.c
+++ drivers/staging/android/sync_debug.c
@@ -34,11 +34,11 @@
  * SYNC_DEBUG
  *
  * [MTK] {{{
- * #define SYNC_DEBUG
+ *#define SYNC_DEBUG
  * [MTK] }}}
  */
 
-/* Defined by MTK for Fence timeout debug, and will not dump at normal time. */
+/*Defined by MTK for Fence timeout debug, and will not dump at normal time.*/
 #define SYNC_DEBUG
 
 static LIST_HEAD(sync_timeline_list_head);
