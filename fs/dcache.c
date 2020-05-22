ceefd21f0feda329c330b7a0b9502cb262e44952
R0rt1z2 <rogerortizleal@gmail.com>
2020-05-22 18:13:36 +0200

    pine: Fix build errors after update sdcardfs.
    Signed-off-by: R0rt1z2 <rogerortizleal@gmail.com>

--- fs/dcache.c
+++ fs/dcache.c
@@ -1574,8 +1574,6 @@ void d_set_d_op(struct dentry *dentry, const struct dentry_operations *op)
 		dentry->d_flags |= DCACHE_OP_DELETE;
 	if (op->d_prune)
 		dentry->d_flags |= DCACHE_OP_PRUNE;
-	if (op->d_select_inode)
-		dentry->d_flags |= DCACHE_OP_SELECT_INODE;
 
 }
 EXPORT_SYMBOL(d_set_d_op);
